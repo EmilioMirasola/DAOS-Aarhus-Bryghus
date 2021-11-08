daa2c5fe6ddd9bfdb4ed3629cb9a2ba5873c3393
Emilio Mirasola <emilio@mirasola.dk>
2021-11-08 11:07:44 +0100

    Create script

---
+++ create.sql
@@ -0,0 +1,58 @@
+create table ProductGroup
+(
+    name           varchar(30) not null unique,
+    productGroupId int identity (1,1) primary key,
+)
+
+create table Product
+(
+    name         varchar(30) not null,
+    productId    int identity (1,1) primary key,
+    stock        int         not null,
+    minimumStock int         not null,
+)
+
+create table PriceList
+(
+    name        varchar(30) not null unique,
+    priceListId int identity (1,1) primary key,
+)
+
+create table ProductPrice
+(
+    price           float not null,
+    discountPercent float,
+    priceListId     int foreign key references PriceList (priceListId),
+    productId       int foreign key references Product (productId),
+    productPriceId  int identity (1,1) primary key,
+)
+
+create table Customer
+(
+    name       varchar(30) not null,
+    customerId int identity (1,1) primary key,
+)
+
+create table Employee
+(
+    name       varchar(30) not null,
+    employeeId int identity (1,1) primary key,
+)
+
+create table Sale
+(
+    customerId int foreign key references Customer (customerId),
+    employeeId int foreign key references Employee (employeeId),
+    saleId     int identity (1,1) primary key ,
+)
+
+create table SalesLine
+(
+    amount         int not null,
+    customPrice    float,
+    productPriceId int foreign key references ProductPrice (productPriceId),
+    saleId         int foreign key references Sale (saleId),
+    salesLineId    int identity (1,1),
+)
+
+
