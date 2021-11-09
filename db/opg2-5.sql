219023ec0e2b38bd58ec661b61d7ff9bdbd1b9f4
Emilio Mirasola <emilio@mirasola.dk>
2021-11-09 11:59:53 +0100

    Opg 2a - 2e

---
+++ db/opg2-5.sql
@@ -0,0 +1,50 @@
+--2a
+select P.name as produkt, price, discountPercent, PL.name as priceList
+from Product P
+         join ProductPrice PP on P.productId = PP.productId
+         join PriceList PL on PL.priceListId = PP.priceListId
+
+--2b
+select SUM(case
+               when customPrice is not null then customPrice
+               when discountPercent is not null
+                   then SL.amount * price * (1 - (discountPercent / 100))
+               else SL.amount * price end) as totalPrice
+from SalesLine SL
+         join ProductPrice PP on SL.productPriceId = PP.productPriceId
+where saleId = 1
+
+--2c
+select name, SUM(amount) as amount
+from Product p
+         join ProductPrice PP on p.productId = PP.productId
+         join SalesLine SL on PP.productPriceId = SL.productPriceId
+         join Sale S on SL.saleId = S.saleId
+where MONTH(date) = 11
+group by name
+having SUM(amount) > 5
+
+--2d
+select P.name as product, PG.name as productGroup
+from Product P
+         join ProductGroup PG on P.productGroupId = PG.productGroupId
+except
+select P.name as product, PG.name as productGroup
+from Product P
+         join ProductGroup PG on PG.productGroupId = P.productGroupId
+         join ProductPrice PP on P.productId = PP.productId
+         join PriceList PL on PL.priceListId = PP.priceListId
+where PL.name = 'Fredagsbar'
+
+--2e
+select AVG(temp.totalPrice) as average
+from (select SUM((case
+                      when customPrice is not null then customPrice
+                      when discountPercent is not null
+                          then SL.amount * price * (1 - (discountPercent / 100))
+                      else SL.amount * price end)) as totalPrice
+      from Sale S
+               join SalesLine SL on S.saleId = SL.saleId
+               join ProductPrice PP on SL.productPriceId = PP.productPriceId
+      where S.saleId = SL.saleId) as temp
+--2f
