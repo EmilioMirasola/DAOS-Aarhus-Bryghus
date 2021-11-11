b77bcfc75b68033bccde96735f5cbe73169cfe79
khizerkhan-2316 <khizer759@gmail.com>
2021-11-11 11:57:56 +0100

    Repository changes

--- src/Opgave_6c.java
+++ src/Opgave_6c.java
@@ -5,96 +5,87 @@ import java.util.Scanner;
 public class Opgave_6c {
 
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        try {
-            int saleId = 0;
-            int price = 0;
-            System.out.println("Vil du tilknytte en salgslinje til et eksisterende salg? indtast ja eller nej");
-            String answer = ReadUtil.readNext();
-            if (answer.equalsIgnoreCase("ja")) {
-                System.out.println("Oversigt over salg: Indtast et salgsID som du vil tilknytte salgslinjen på");
-                ResultSet sales = Repository.getAllSales();
-                if (sales != null) {
-                    while (sales.next()) {
-                        System.out.println("dato: " + sales.getString(3) + ", salgsid: " + sales.getString(4));
-                    }
-                    saleId = ReadUtil.readInt();
-                }
+		try {
+			int saleId = 0;
+			int customPrice = 0;
+			System.out.println("Vil du tilknytte en salgslinje til et eksisterende salg? indtast ja eller nej");
+			String answer = ReadUtil.readNext();
+			if (answer.equalsIgnoreCase("ja")) {
+				System.out.println("Oversigt over salg: Indtast et salgsID som du vil tilknytte salgslinjen på");
+				ResultSet sales = Repository.getAllSales();
+				if (sales != null) {
+					while (sales.next()) {
+						System.out.println("dato: " + sales.getString(3) + ", salgsid: " + sales.getString(4));
+					}
+					saleId = ReadUtil.readInt();
+				}
 
-            }
-
-
-            System.out.println("Indtast antal");
-            int antal = ReadUtil.readInt();
-
-            System.out.println("Vil du angive en selvangivet pris?, Angiv venligst ja eller nej");
-            String reply = ReadUtil.readNext();
-
-            if (reply.equalsIgnoreCase("ja")) {
-                System.out.println("Indtast pris");
-                price = ReadUtil.readInt();
-            }
-
-            System.out.println("Her er en række priser, indtast productPriceId for at vælge en pris:");
-            Statement productPrice = connection.createStatement();
-            productPrice.executeQuery("select price, discountPercent, productPriceId from ProductPrice");
-            ResultSet rs = productPrice.getResultSet();
-            while (rs.next()) {
-                System.out.println("ProductPrideId: " + rs.getInt("productPriceId") + ", pris: " + rs.getInt("price") + ", rabat:  " + rs.getInt("discountPercent"));
-            }
-            int productPriceId = ReadUtil.readInt();
-
-            String createSalesLine = null;
-
-            if (price > 0 && answer.equalsIgnoreCase("ja")) {
-                createSalesLine = "insert into SalesLine values(?,?,?,?)";
-            } else if (price > 0) {
-                createSalesLine = "insert into SalesLine values(?,?,?,2)";
-            } else {
-                createSalesLine = "insert into SalesLine values(?,null,?,2)";
-            }
-
-            PreparedStatement preparedStatement = connection.prepareStatement(createSalesLine);
-
-            if (price > 0 && answer.equalsIgnoreCase("ja")) {
-                preparedStatement.setInt(1, antal);
-                preparedStatement.setFloat(2, price);
-                preparedStatement.setInt(3, productPriceId);
-                preparedStatement.setInt(4, saleId);
-            } else if (price > 0) {
-                preparedStatement.setInt(1, antal);
-                preparedStatement.setFloat(2, price);
-                preparedStatement.setInt(3, productPriceId);
-            } else {
-                preparedStatement.setInt(1, antal);
-                preparedStatement.setInt(2, productPriceId);
-
-            }
-
-            int rowsaffected = preparedStatement.executeUpdate();
-            System.out.println("Rows afftected: " + rowsaffected);
-
-            Opgave_6c.checkStock(connection, productPriceId);
-            connection.close();
+			}
 
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+			System.out.println("Indtast antal");
+			int amount = ReadUtil.readInt();
 
-    public static void checkStock(Connection connection, int productPriceId) {
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultset = statement.executeQuery("SELECT * FROM stock_check(" + productPriceId + ")");
-            while (resultset.next()) {
-                if (resultset.getInt(1) < resultset.getInt(2)) {
-                    System.out.println("Der er mindre end minimum antal på produktet:");
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+			System.out.println("Vil du angive en selvangivet pris?, Angiv venligst ja eller nej");
+			String reply = ReadUtil.readNext();
+
+			if (reply.equalsIgnoreCase("ja")) {
+				System.out.println("Indtast pris");
+				customPrice = ReadUtil.readInt();
+			}
+
+			System.out.println("Her er en række priser, indtast productPriceId for at vælge en pris:");
+			ResultSet getAllPrices = Repository.getAllProductPrices();
+			while (getAllPrices.next()) {
+				System.out.println("ProductPrideId: " + getAllPrices.getInt("productPriceId") + ", pris: " + getAllPrices.getInt("price") + ", rabat:  " + getAllPrices.getInt("discountPercent"));
+			}
+			int productPriceId = ReadUtil.readInt();
+
+			String createSalesLine = null;
+
+			if (customPrice > 0 && answer.equalsIgnoreCase("ja")) {
+				createSalesLine = "insert into SalesLine values(?,?,?,?)";
+			} else if (customPrice > 0) {
+				createSalesLine = "insert into SalesLine values(?,?,?,2)";
+			} else {
+				createSalesLine = "insert into SalesLine values(?,null,?,2)";
+			}
+
+			PreparedStatement preparedStatement = connection.prepareStatement(createSalesLine);
+
+			if (customPrice > 0 && answer.equalsIgnoreCase("ja")) {
+				preparedStatement.setInt(1, amount);
+				preparedStatement.setFloat(2, customPrice);
+				preparedStatement.setInt(3, productPriceId);
+				preparedStatement.setInt(4, saleId);
+			} else if (customPrice > 0) {
+				Repository.createSaleWithCustomPrice(amount,customPrice, productPriceId );
+			} else {
+				Repository.createSale(amount, productPriceId);
+			}
+
+
+			Opgave_6c.checkStock(connection, productPriceId);
+
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void checkStock(Connection connection, int productPriceId) {
+		try {
+			Statement statement = connection.createStatement();
+			ResultSet resultset = statement.executeQuery("SELECT * FROM stock_check(" + productPriceId + ")");
+			while (resultset.next()) {
+				if (resultset.getInt(1) < resultset.getInt(2)) {
+					System.out.println("Der er mindre end minimum antal på produktet:");
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/Repository.java
+++ src/Repository.java
@@ -4,64 +4,150 @@ import java.util.Optional;
 public class Repository {
 
 
-    public static void printProductGroups() throws SQLException {
-        Optional<Connection> connection = getConnection();
-        if (connection.isPresent()) {
-            Statement stmt = connection.get().createStatement();
-            ResultSet result = stmt.executeQuery("select *  from ProductGroup");
-            System.out.println("Produktgrupper:");
-            while (result.next()) {
-                System.out.println(result.getString(1) + ", produktgruppeID: " + result.getInt(2));
-            }
-        }
-        System.out.println();
-    }
+	public static void printProductGroups() throws SQLException {
+		Optional<Connection> connection = getConnection();
+		if (connection.isPresent()) {
+			Statement stmt = connection.get().createStatement();
+			ResultSet result = stmt.executeQuery("select *  from ProductGroup");
+			System.out.println("Produktgrupper:");
+			while (result.next()) {
+				System.out.println(result.getString(1) + ", produktgruppeID: " + result.getInt(2));
+			}
+		}
+		System.out.println();
+	}
 
-    public static void printProducts(int id) throws SQLException {
-        Optional<Connection> connection = getConnection();
-        if (connection.isPresent()) {
-            Statement stmt = connection.get().createStatement();
-            ResultSet result = stmt.executeQuery("select name, productId  from Product P where P.productGroupId =" + id);
+	public static void printProducts(int id) throws SQLException {
+		Optional<Connection> connection = getConnection();
+		if (connection.isPresent()) {
+			Statement stmt = connection.get().createStatement();
+			ResultSet result = stmt.executeQuery("select name, productId  from Product P where P.productGroupId =" + id);
 
-            System.out.println();
-            System.out.println("Produkter:");
-            while (result.next()) {
-                System.out.println(result.getString(1) + ", produktID: " + result.getInt(2));
-            }
-            System.out.println();
-        }
-    }
+			System.out.println();
+			System.out.println("Produkter:");
+			while (result.next()) {
+				System.out.println(result.getString(1) + ", produktID: " + result.getInt(2));
+			}
+			System.out.println();
+		}
+	}
 
-    public static ResultSet getTotalSaleInDKKForASpeceficProduct(int productId, String date) throws SQLException {
-        Optional<Connection> connection = getConnection();
-        if (connection.isPresent()) {
-            Statement statement = connection.get().createStatement();
-            return statement.executeQuery("exec getTotalSaleInDKKForASpeceficProduct " + productId + ", " + "'" + date + "'");
-        }
-        return null;
-    }
+	public static ResultSet getTotalSaleInDKKForASpeceficProduct(int productId, String date) throws SQLException {
+		Optional<Connection> connection = getConnection();
+		if (connection.isPresent()) {
+			Statement statement = connection.get().createStatement();
+			return statement.executeQuery("exec getTotalSaleInDKKForASpeceficProduct " + productId + ", " + "'" + date + "'");
+		}
+		return null;
+	}
 
-    private static Optional<Connection> getConnection() {
-        try {
-            return Optional.of(DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLExpress:1433;database=AarhusBryghus_DB;user=sa;password=" + System.getenv("DB_PASSWORD")));
-        } catch (SQLException e) {
-            return Optional.empty();
-        }
-    }
+	private static Optional<Connection> getConnection() {
+		try {
+			return Optional.of(DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLExpress:1433;database=AarhusBryghus_DB;user=sa;password=" + System.getenv("DB_PASSWORD")));
+		} catch (SQLException e) {
+			return Optional.empty();
+		}
+	}
 
-    public static ResultSet getAllSales() {
-        Optional<Connection> connection = getConnection();
+	public static ResultSet getAllSales() {
+		Optional<Connection> connection = getConnection();
 
-        if (connection.isPresent()) {
-            try {
-                Statement statement = connection.get().createStatement();
-                ResultSet resultset = statement.executeQuery("SELECT * FROM Sale");
-                return resultset;
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+		if (connection.isPresent()) {
+			try {
+				Statement statement = connection.get().createStatement();
+				ResultSet resultset = statement.executeQuery("SELECT * FROM Sale");
+				return resultset;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 
-        } else {
-            return null;
-        }
-    }
+
+		}
+
+		return null;
+	}
+
+	public static ResultSet getAllProductPrices() {
+		Optional<Connection> connection = getConnection();
+
+		if (connection.isPresent()) {
+			try {
+				Statement statement = connection.get().createStatement();
+				ResultSet resultset = statement.executeQuery("select price, discountPercent, productPriceId from ProductPrice");
+
+				return resultset;
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+
+		}
+		return null;
+	}
+
+	public static Integer createSale(int amount, int productPriceId) {
+		Optional<Connection> connection = getConnection();
+
+		if (connection.isPresent()) {
+			String createSalesLine = "insert into SalesLine values(?,null,?,2)";
+
+			try {
+				PreparedStatement statement = connection.get().prepareStatement(createSalesLine);
+				statement.setInt(1, amount);
+				statement.setInt(2, productPriceId);
+
+				return statement.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return null;
+	}
+
+
+
+	public static Integer createSaleWithCustomPrice(int amount, int productPriceId, float customPrice) {
+		Optional<Connection> connection = getConnection();
+
+		if (connection.isPresent()) {
+			String createSalesLine = "insert into SalesLine values(?,?,?,2)";
+
+			try {
+				PreparedStatement statement = connection.get().prepareStatement(createSalesLine);
+				statement.setInt(1, amount);
+				statement.setFloat(2, customPrice);
+				statement.setInt(3, productPriceId);
+
+				return statement.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return null;
+	}
+
+	public static Integer createSaleWithCustomPriceAndNewSale(int amount, int productPriceId, float customPrice) {
+		Optional<Connection> connection = getConnection();
+
+		if (connection.isPresent()) {
+			String createSalesLine = "insert into SalesLine values(?,?,?,2)";
+
+			try {
+				PreparedStatement statement = connection.get().prepareStatement(createSalesLine);
+				statement.setInt(1, amount);
+				statement.setFloat(2, customPrice);
+				statement.setInt(3, productPriceId);
+
+				return statement.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+		return null;
+	}
+
+
+}
