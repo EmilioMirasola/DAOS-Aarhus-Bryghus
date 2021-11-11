6fb2075846d53866fe955642d74d21fecd71b43c
Emilio Mirasola <emilio@mirasola.dk>
2021-11-11 10:54:41 +0100

    6b fiks

--- src/Opgave_6b.java
+++ src/Opgave_6b.java
@@ -4,33 +4,59 @@ import java.util.Scanner;
 
 public class Opgave_6b {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // fix denne, så den fungere
-        try{
+		// fix denne, så den fungere
+		try {
+			Connection connection;
+			Scanner scanner = new Scanner(System.in);
+			connection = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLExpress:1433;database=AarhusBryghus_DB;user=sa;password=" + System.getenv("DB_PASSWORD"));
 
-            Connection connection;
-            Scanner scanner = new Scanner(System.in);
-            connection = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLExpress:1433;database=AarhusBryghus_DB;user=sa;password=" + System.getenv("DB_PASSWORD"));
+			printProductGroups(connection);
 
-            System.out.println("Indtast det produkt du vil se det samlede salg i DKK");
+			System.out.println("Indtast ID på den produktgruppe, du vil se produkter for");
+			int productGroupId = ReadUtil.readInt();
 
-            String productName = scanner.nextLine();
-            System.out.println("Indtast dato");
-            LocalDate date = LocalDate.parse(scanner.nextLine());
+			printProducts(connection, productGroupId);
 
+			System.out.println("Indtast ID på det produkt du vil se det samlede salg for i DKK");
+			int productId = ReadUtil.readInt();
 
+			System.out.println("Indtast dato");
+			String date = scanner.nextLine();
 
-            Statement statement = connection.createStatement();
-           ResultSet result = statement.executeQuery("exec getTotalSaleInDKKForASpeceficProduct " + productName + ", " + date);
+			Statement statement = connection.createStatement();
+			ResultSet totalSale = statement.executeQuery("exec getTotalSaleInDKKForASpeceficProduct " + productId + ", " + "'" + date + "'");
 
-           while(result.next()){
-               System.out.println("Produkt: " + productName + "\nSamlet salg i DKK: " + result.getString(1));
-           }
+			while (totalSale.next()) {
+				System.out.println("\nSamlet salg i DKK: " + totalSale.getString(1));
+			}
 
-        }catch(SQLException e){
-            e.printStackTrace();
-        }
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
-    }
+	}
+
+	private static void printProductGroups(Connection connection) throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet result = stmt.executeQuery("select *  from ProductGroup");
+		System.out.println("Produktgrupper:");
+		while (result.next()) {
+			System.out.println(result.getString(1) + ", produktgruppeID: " + result.getInt(2));
+		}
+		System.out.println();
+	}
+
+	private static void printProducts(Connection connection, int id) throws SQLException {
+		Statement stmt = connection.createStatement();
+		ResultSet result = stmt.executeQuery("select name, productId  from Product P where P.productGroupId =" + id);
+
+		System.out.println();
+		System.out.println("Produkter:");
+		while (result.next()) {
+			System.out.println(result.getString(1) + ", produktID: " + result.getInt(2));
+		}
+		System.out.println();
+	}
 }
